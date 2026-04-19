f95d534e8980bfd645c80255d38a74e775e8664c
alilahdab <sanaabalsam1980@gmail.com>
2026-04-19 04:15:58 +0100

    Fix: ajout reel des fichiers C et Python

---
+++ Projets_Python/calculator_py.py
@@ -0,0 +1,25 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    if b!=0:
+        return a/b
+    return "erreure"
+while True:
+    a = float(input("Enter a number: "))
+    op = input("enter an operator(+,-,*,/):")
+    b = float(input("Enter another number: "))
+    if op=="+":
+     print(add(a,b))
+    elif op=="-":
+     print(sub(a,b))
+    elif op=="*":
+        print(mul(a,b))
+    elif op=="/":
+        print(div(a,b))
+    else:
+        print("error")
+
